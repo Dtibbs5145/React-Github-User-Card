8b9f5e6d50647da8afaf21327fe1c82ee642a063
Destiny Tibbitts <dtibbs5145@gmail.com>
2019-11-06 00:45:28 -0500

    MVP hit

--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -3,11 +3,11 @@ import React from 'react';
 const UserCard = (props) => {
     return (
         <div className='App'>
+            <h3>User: {props.user.name}</h3>
+            <img src={props.user.avatar_url} alt={props.user.name} />
             <p>Their username is {props.user.login}</p>
-            <p>They have {props.user.followers} followers</p>
             <p>They are following {props.user.following} person</p>
             <p>Their bio says: {props.user.bio}</p>
-            <p>They started using Github on {props.user.created_at}</p>
         </div>
     )
 }
